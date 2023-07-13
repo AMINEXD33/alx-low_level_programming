22d229aa16390f2e8f57be91871c36cf2f730cb2
Amine Meftah <amine.gamer222@gmail.com>
2023-07-13 18:24:58 +0100

    printf sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Description: 'print all the size of the variable , depending on the system 
+ *
+ * Description: 'print all the size of the variable , depending on the system
  *(32 or 64)'
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+	int sizes[] = {sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof (float)};
+	char *sentences[] = {"a char", "an int", "a long", "a long long integer", "a float"};
+	for (int i = 0; i < sizeof(sizes) / sizeof(sizes[0]); i++)
+	{
+		printf("Size of %s: %d\n", sentences[i], sizes[i]);
+	};
+	return	(0);
 }
