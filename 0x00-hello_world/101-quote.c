9c1fbae1b7eb06cd00dce93e1006a717e7005f55
Amine Meftah <amine.gamer222@gmail.com>
2023-07-13 18:48:59 +0100

    to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 1 (an error throw)
+ */
+int main(void)
+{
+	fprintf(stderr
+			, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
