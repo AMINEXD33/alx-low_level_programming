5c70c75a0b6d2adf30228604bf57075079f3cbd7
Amine Meftah <amine.gamer222@gmail.com>
2023-07-13 16:28:33 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main(){
+/* print what ever that is and return 0 is everything went good*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return 0;
-
+	(return 0);
 }
