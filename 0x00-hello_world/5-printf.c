e6cb557a07649c3cf319bcfd6b3e6985b48b6d16
Amine Meftah <amine.gamer222@gmail.com>
2023-07-13 16:50:44 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char txt[] = "with proper grammar, but the outcome is a piece of art";
+
+	printf(" % s", txt);
+	return (0);
+}
