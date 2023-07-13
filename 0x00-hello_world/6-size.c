0a781d7ee0d27f43db88acf6f962f0da75f1ddc4
Amine Meftah <amine.gamer222@gmail.com>
2023-07-13 18:29:11 +0100

    printf sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	int i;
 	int sizes[] = {sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof (float)};
 	char *sentences[] = {"a char", "an int", "a long", "a long long integer", "a float"};
-	for (int i = 0; i < sizeof(sizes) / sizeof(sizes[0]); i++)
+	for (i = 0; i < sizeof(sizes) / sizeof(sizes[0]); i++)
 	{
 		printf("Size of %s: %d\n", sentences[i], sizes[i]);
 	};
