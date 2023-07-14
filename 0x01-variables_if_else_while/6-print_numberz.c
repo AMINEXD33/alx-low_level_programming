a90ea8f494b0478d2ce752f02d9a9e0d0ead5a69
Amine Meftah <amine.gamer222@gmail.com>
2023-07-14 20:29:03 +0100

    0-9 char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,10 @@
 int main(void)
 {
 	int tracker;
-	char tmp_char[10];
 
 	for (tracker = 0 ; tracker < 10 ; tracker++)
 	{
-		tmp_char[tracker] = '0' + tracker;
+		putchar('0' + tracker);
 	}
-	for (tracker = 0 ; tracker < 10 ; tracker++)
-	{
-		putchar(tmp_char[tracker]);
-	}
-	putchar('\n');
 	return (0);
 }
