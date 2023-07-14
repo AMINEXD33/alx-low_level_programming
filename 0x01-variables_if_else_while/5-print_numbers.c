e080dc34865b191efae3ba03a150441c1c80bb80
Amine Meftah <amine.gamer222@gmail.com>
2023-07-14 20:12:56 +0100

    a-z  - (q and e)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int tracker;
+
+	for (tracker = 0 ; tracker < 10 ; tracker++)
+	{
+		printf("%d", tracker);
+	}
+	return (0);
+}
