3a4362a32be25a3ec033e2f004ac17ea5dabb988
Amine Meftah <amine.gamer222@gmail.com>
2023-07-15 00:57:21 +0100

    by far this was the hardest one so far , sheesh

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,9 +15,9 @@ int main(void)
 	{
 		for (num1_2 = 0; num1_2 <= 9; num1_2++)
 		{
-			for (num2_1 = num1_1; num2_1 <= 9; num2_1++)
+			for (num2_1 = 0; num2_1 <= 9; num2_1++)
 			{
-				for (num2_2 = 0; num2_2 <= 9; num2_2++)
+				for (num2_2 = 1 ; num2_2 <= 9; num2_2++)
 				{
 					putchar(num1_1 + '0');
 					putchar(num1_2 + '0');
