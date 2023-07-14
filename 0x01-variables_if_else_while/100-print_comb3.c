5ec69a174c1f57d2144bcdffc280a0fa77b234fc
Amine Meftah <amine.gamer222@gmail.com>
2023-07-14 22:37:06 +0100

    all possible combos with two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	int tracker;
+	int tracker2;
+
+	for (tracker = 0 ; tracker < 10 ; tracker++)
+	{
+		for (tracker2 = 1 ; tracker2 < 10 ; tracker2++)
+		{
+			if (tracker != 0 ||  tracker2 != 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar('0' + tracker);
+			putchar('0' + tracker2);
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
