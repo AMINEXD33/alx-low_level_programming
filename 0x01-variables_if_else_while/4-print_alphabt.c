e47da34f48ccaadd2356cea5b73fbfa794456f5e
Amine Meftah <amine.gamer222@gmail.com>
2023-07-14 19:50:13 +0100

    a-z  - (q and e)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[26];
+	int tracker;
+
+	for (tracker = 0 ; tracker < 26 ; tracker++)
+	{
+		if (('a' + tracker) != 'q' && ('a' + tracker) != 'e')
+		{
+			alphabet[tracker] = 'a' + tracker;
+		}
+	}
+	for (tracker = 0 ; tracker < 26 ; tracker++)
+	{
+		printf("%c", alphabet[tracker]);
+	}
+	printf("\n");
+	return (0);
+}
+
