72da645ee41bb063f5462237c3aba107af8ee55c
Amine Meftah <amine.gamer222@gmail.com>
2023-07-14 21:48:35 +0100

    alphabet[::-1]

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[26];
+	int tracker;
+
+	for (tracker = 0; tracker < 26 ; tracker++)
+	{
+		alphabet[tracker] = 'a' + tracker;
+	}
+	for (tracker = 26 ; tracker >= 0 ; tracker--)
+	{
+		printf("%c", alphabet[tracker]);
+	}
+	printf("\n");
+	return (0);
+}
+
