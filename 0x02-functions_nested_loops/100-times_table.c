5f2b18c32d62514ffcfb474fb05600cc032c023e
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 20:03:42 +0100

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,38 +11,17 @@ void print_times_table(int n)
 {
 	int n1;
 	int n2;
-	int result;
 
-	if (n > 0 || n < 15)
+	if (n > 15 || n < 0)
 	{
-		for (n1 = 0; n1 <= n; n1++)
+		return;
+	}
+	for (n1 = 0; n1 <= n; n1++)
+	{
+		for (n2  = 0; n2 <= n; n2++)
 		{
-			for (n2 = 0; n2 <= n; n2++)
-			{
-				result = n1 * n2;
-				if ((n2 != 0 && n2 != n) && (n <= 15 && n >= 0))
-				{
-					if (result < 10)
-					{
-						printf("   %d,", result);
-					}
-					else if (result > 10 && result < 100)
-					{
-						printf("  %d,", result);
-					}
-					else if (result >= 100 && result < 1000)
-					{
-						printf(" %d,", result);
-					}
-				}
-				else if ((n2 == 0 ||  n2 == n) && (n <= 15 && n >= 0))
-				{
-					printf("%d", result);
-				}
-			}
-		if (n <= 15 && n >= 0)
-			printf("\n");
+			printf("%-4d", n1 * n2);
 		}
+		printf("\n");
 	}
 }
-
