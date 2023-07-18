0c50cf9d08e863f3173bf2bed6c3aaafbf721c34
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 23:25:50 +0100

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: 'print the first 50 units of fibonacci
+*
+* Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long a;
+	long b;
+	long int sum;
+	int x;
+
+	a = 0;
+	b = 1;
+	sum = 0;
+	for (x = 0 ; x <= 50 ; x++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if (x < 50)
+		{
+			printf("%ld, ", sum);
+		}
+		else if (x == 50)
+		{
+			printf("%ld", sum);
+		}
+	}
+	return (0);
+}
