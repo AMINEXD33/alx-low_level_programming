2cd5f058b21d0ac2045bf6729031e985258c57fb
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 13:18:43 +0100

    return the abs value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _abs- _abs(-1) =  1
+*
+*Description: 'figure out the sign of the number
+*and bassed on it get's the abs value'
+*@n : is the number passed
+* Return: the abs value of a number
+*/
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -n);
+	}
+}
