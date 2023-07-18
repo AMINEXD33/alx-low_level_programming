b3a1942f51cf2bb3641d3517dcbbe2ab63ab77b0
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 18:17:42 +0100

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		n--;
 		while (n != 98)
 		{
-			printf(" %d,", n);
+			printf(", %d,", n);
 			n--;
 		}
 		printf("%d", n);
@@ -26,9 +26,9 @@ void print_to_98(int n)
 		n++;
 		while (n != 98)
 		{
-			printf(" %d,", n);
+			printf(", %d,", n);
 			n++;
 		}
-		printf("%d,", n);
+		printf("%d", n);
 	}
 }
