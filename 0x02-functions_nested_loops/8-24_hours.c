d104cb44ad465021c27bfd62bd52b2c40292d4ad
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 15:00:45 +0100

    jacke s day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,26 +8,18 @@
 void jack_bauer(void)
 {
 	int hours_1;
-	int hours_2;
 	int min_1;
-	int min_2;
 
-	for (hours_1 = 0; hours_1 <= 2; hours_1++)
+	for (hours_1 = 0; hours_1 < 24; hours_1++)
 	{
-		for (hours_2 = 0; hours_2 <= 3; hours_2++)
+		for (min_1 = 0; min_1 < 60; min_1++)
 		{
-			for (min_1 = 0; min_1 <= 5; min_1++)
-			{
-				for (min_2 = 0; min_2 <= 9; min_2++)
-				{
-					_putchar('0' + hours_1);
-					_putchar('0' + hours_2);
-					_putchar(':');
-					_putchar('0' + min_1);
-					_putchar('0' + min_2);
-					_putchar('\n');
-				}
-			}
+			_putchar('0' + (hours_1 / 10));
+			_putchar('0' + (hours_1 % 10));
+			_putchar(':');
+			_putchar('0' + (min_1 / 10));
+			_putchar('0' + (min_1 % 10));
+			_putchar('\n');
 		}
 	}
 }
