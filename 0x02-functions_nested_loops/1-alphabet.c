ac73044373a6d3b3c575a18e58f6d8ecb665932e
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 11:19:35 +0100

    print the alphabet and a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: 'print the alphabet'
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
