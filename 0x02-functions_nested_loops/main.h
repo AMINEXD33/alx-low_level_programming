bd495c2598bc5606ffe570cb4e9696783f838ed9
Amine Meftah <amine.gamer222@gmail.com>
2023-07-18 11:56:17 +0100

    print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,16 +2,3 @@ int _putchar(char c);
 
 
 
-void print_alphabet(void)
-{
-        int i;
-        char alpha = 'a';
-
-        for (i = 0; i <= 25; i++)
-        {
-                _putchar(alpha + i);
-        }
-        _putchar('\n');
-}
-
-
