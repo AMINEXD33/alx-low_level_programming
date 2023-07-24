f6bd2e82224e611c49b4608dd22126c79fbab4ca
Amine Meftah <amine.gamer222@gmail.com>
2023-07-24 20:27:30 +0100

    print a char str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a char str and a new line
+ * @str : is the char variable
+ * Return: None
+ */
+void _puts(char *str)
+{
+	int counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		_putchar(str[counter]);
+	}
+	_putchar('\n');
+}
