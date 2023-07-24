f6f13d535bd31b575ea35670138a2d573006d921
Amine Meftah <amine.gamer222@gmail.com>
2023-07-25 00:32:25 +0100

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * is_num - a determine if the char represent
+ * Description: from ascii code determine if the
+ * value of the char represent a number or not
+ * a number
+ * @X: a char
+ * Return: 1 if yes , 0 if no
+ */
+int is_num(char X)
+{
+	if (X <= 57 && X >= 48)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * _atoi - determine the first number and its signe in a string
+ * Description: get the first number and its signe from any list of chars
+ * of length n
+ *@s : char list (array)
+ *Return:the first int found and its sign
+ */
+int _atoi(char *s)
+{
+	int i;
+	int negative;
+	int value;
+
+	negative = 0;
+	i = 0;
+	value = 0;
+	while (s[i] != '\0')
+	{
+		if (is_num(s[i]) == 1)
+		{
+			while (1)
+			{
+				if (is_num(s[i]) == 0 || s[i] == '\0')
+				{
+					if ((negative % 2) == 1)
+						return (value * -1);
+					else if ((negative % 2) == 0)
+						return (value);
+				}
+				value = value * 10 + (s[i] - '0');
+				i++;
+			}
+
+		}
+		else if (s[i] == 45)
+		{
+			negative++;
+		}
+		i++;
+	}
+	return (value);
+}
+
+
+
+
+
+
+
+
+
+
+
