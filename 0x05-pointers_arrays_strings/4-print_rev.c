4de778a958a9ee887b9a9175a162a29f54a2c7c1
Amine Meftah <amine.gamer222@gmail.com>
2023-07-24 20:39:10 +0100

    print a char str in reverce

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_rev - print a char str in reverce
+ *@s : is the variable char
+ *Return: None
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	while (len != 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
