0ca7655de7f770e5d236811ba62d116e1789d0c2
Amine Meftah <amine.gamer222@gmail.com>
2023-07-24 21:42:55 +0100

    print every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *puts2 - put every other char of a string
+ *@str : char var
+ *Return: None
+ *
+ */
+void puts2(char *str)
+{
+	int tracker;
+	int X;
+	int  flag;
+
+	flag = 1;
+	X = 0;
+	tracker = 0;
+	while (str[X] != '\0')
+	{
+		if (flag == 1)
+		{
+			_putchar(str[X]);
+			flag = 0;
+		}
+
+		if (tracker == 2)
+		{
+			_putchar(str[X]);
+			tracker = 0;
+		}
+		X++;
+		tracker++;
+
+	}
+}
