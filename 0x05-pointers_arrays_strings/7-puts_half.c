120f91eb7f9bbde04fec0f551b76333dd7d53a11
Amine Meftah <amine.gamer222@gmail.com>
2023-07-24 21:54:32 +0100

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *puts_half - print the second half of a char list
+ *@str: is the char var
+ *
+ *Return: None
+ *
+ */
+void puts_half(char *str)
+{
+	int len;
+	int tracker;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len > 0)
+	{
+		len--;
+		if (((len / 2) % 2)  == 0)
+		{
+			tracker = len / 2;
+		}
+		else
+		{
+			traker = (len - 1) / 2;
+		}
+		while (str[tracker] != '\0')
+		{
+			_putchar(str[tracker]);
+		}
+		_putchar('\n');
+	}
+}
