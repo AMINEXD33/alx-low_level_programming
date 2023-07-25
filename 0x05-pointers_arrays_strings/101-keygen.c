8de305ca9aa5447798e6adcd0f31edfba92ffd2b
Amine Meftah <amine.gamer222@gmail.com>
2023-07-25 02:04:33 +0100

    crack the password 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+ *generate - generate a password for 101-keygen.c
+ *
+ *Description: after looking at the assembly code and reverce 
+ *engineering it i was looking at a if statmenr that basically 
+ *goes throught  a string , and as it go it sums the ascii value of 
+ *every letter and and based on it >>> if (sum == 2772) it passes
+ *so my goes is to just generate a string collection that sums to 2772
+ *doing this (2772 % (ASCII CODE OF A LETTER)) , for example i've got 0
+ *in this exampe by using "B", so i just generated X*"B" that ,validate the 
+ *if statment giving us ("B"*42)  = 2772
+ */
+void  generate()
+{
+    
+    int value = 0;
+    int standard = (int) * "B";
+    int count = 0;
+    
+    while (value <= 2772)
+    {
+        if (value + standard > 2772)
+            break;
+        value = value + standard;
+        count++;
+    }
+        
+    char password[count+1];
+    int counter = 0;
+    while (counter <= count)
+    {
+        if (count == counter)
+        {
+            password[counter] = '\0';
+            break;
+        }
+        password[counter] = 'B';
+        counter++;
+    }
+   printf("%s", password);
+}
+/**
+ *main - entry point
+ *
+ *Return:Always (0)
+ *
+ */
+int main()
+{
+	generate();
+	return (0);
+}
