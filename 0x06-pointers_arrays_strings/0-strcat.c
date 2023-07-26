da323dcf515868ac1a4e53060799a3d43eb4dc37
Amine Meftah <amine.gamer222@gmail.com>
2023-07-26 19:09:27 +0100

    strcat two strings !

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
+ * *_strcat - concatenates a string to another  src - > dest
  *
+ *@dest: destination string
+ *@src: source string
  *
- *
- *
- *
+ * Return: the pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int cursor;
 	int cursorII;
-	
+
 	cursorII = 0;
 	cursor = 0;
 	while (1)
@@ -19,14 +20,14 @@ char *_strcat(char *dest, char *src)
 		{
 			dest[cursorII] = ' ';
 			cursorII++;
-			while(src[cursor] != '\0')
+			while (src[cursor] != '\0')
 			{
 				dest[cursorII] = src[cursor];
 				cursor++;
 				cursorII++;
 			}
 			dest[cursorII] = '\0';
-			break;	
+			break;
 		}
 		cursorII++;
 	}
