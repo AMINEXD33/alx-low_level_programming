e8a94587773cb3ac1dfafc5ec6d110aeab9b565d
Amine Meftah <amine.gamer222@gmail.com>
2023-07-27 17:51:44 +0100

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,9 +7,34 @@
  */
 void print_number(int n)
 {
-	while (n > 0)
+	int flag;
+	int flag2;
+
+	if (n == 0)
 	{
-		_putchar((n%10)+'0');
+		_putchar('0');
+		return;
+	}
+	flag = 0;
+	flag2 = 0;
+	if (n < 0)
+	{
+		n = n*-1;
+		flag = 1;
+	}
+	while (n >0)
+	{
+		if (flag == 1){
+			if (flag2 == 0){
+				_putchar('-');
+				flag2 = 1;
+			}
+			_putchar((n % 10) + '0');
+		}
+		else
+		{
+			_putchar((n % 10) + '0');
+		}
 		n /= 10;
 	}
 }
