cfc0a529f0b32a25c72f27cc2382bc656e755309
Amine Meftah <amine.gamer222@gmail.com>
2023-07-27 18:11:25 +0100

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,45 +9,20 @@ void print_number(int n)
 {
 	int flag;
 	int flag2;
-	if (n == 2147483647)
-	{
-		_putchar('2');
-		_putchar('1');
-		_putchar('4');
-		_putchar('7');
-		_putchar('4');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('7');
-		_putchar('4');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('7');
-		_putchar('0');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('7');
-		return;
-	}
+
 	if (n == 0)
 	{
-		_putchar('0');
+		_putchar(n + '0');
 		return;
 	}
 	flag = 0;
 	flag2 = 0;
 	if (n < 0)
 	{
-		n = n*-1;
+		n = n * -1;
 		flag = 1;
 	}
-	while (n >0)
+	while (n > 0)
 	{
 		if (flag == 1){
 			if (flag2 == 0){
