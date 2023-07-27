7c5363c8d1d417870a27c3d166ec67a6196b6e2b
Amine Meftah <amine.gamer222@gmail.com>
2023-07-27 17:10:16 +0100

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * helper_function - this function just calculates the
- * position of the next 13nth letter
- *@C: is the position of a letter
- *Return: the position of the 13nth
- */
-int helper_function(int *C)
-{
-	if (*C + 13 < 26)
-	{
-		return (*C + 13);
-	}
-	else if (*C + 13 >= 26)
-	{
-		return ((*C + 13) - 26);
-	}
-	return (-1);
-}
-/**
  * *rot13 - this function encodes a text by switching every letter by
  * next 13 position relative to it .
  *@s: the string
@@ -30,7 +12,7 @@ char *rot13(char *s)
 {
 	int cur;
 	int cur2;
-	char dict[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char dict[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 	cur = 0;
 	while (s[cur] != '\0')
@@ -41,12 +23,7 @@ char *rot13(char *s)
 		{
 			if (s[cur] == dict[cur2])
 			{
-				s[cur] = (helper_function(&cur2) + 'A');
-				break;
-			}
-			else if (s[cur] == dict[cur2] + 32)
-			{
-				cur[s] = ((helper_function(&cur2) + 'A') + 32);
+				s[cur] = dict[((cur2 + 26) % 52)];
 				break;
 			}
 			cur2++;
