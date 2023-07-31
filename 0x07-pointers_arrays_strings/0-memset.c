5d618057adb64a0014eff5c25ab3edc91284c477
Amine Meftah <amine.gamer222@gmail.com>
2023-07-31 11:04:18 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - fill the first n bytes with b, target is the s
+ *@s: buffer array
+ *@b: the value the function is using to fill
+ *@n:number of bytes
+ *Return: the adress to the buffer array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n >= 0)
+	{
+		s[n] = b;
+		n--;
+	}
+	return (s);
+}
