26cb0dbd9ac2e46ed983b9ce14cf662b79c853ef
Amine Meftah <amine.gamer222@gmail.com>
2023-07-31 20:35:20 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,39 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h>
-/**
-* checker - checks is the the following bytes are exactly equal to needle
-*@haystack: string we're searching in
-*@position: is the position of the first needle byte
-*@needle: is the needle word ,, aka what we're searching for
-*Return: 0 if needle does not eq to the string , starting at position
-*/
-
-int checker(char *haystack, int position, char *needle)
-{
-	int x;
-	int flag;
-
-	flag = 1;
-	x = 0;
-	while (needle[x] != '\0' && haystack[x + position] != '\0')
-	{
-		if (haystack[position + x] != needle[x])
-		{
-			flag = 0;
-			break;
-		}
-		x++;
-		if (haystack[position + x] == '\0' && needle[x] != '\0')
-		{
-			flag = 0;
-			break;
-		}
-	}
-	return (flag);
-}
-
 /**
  * *_strstr - loops over string and find the first occurence of a needle
  *@haystack: the string we're searching in
@@ -41,20 +8,18 @@ int checker(char *haystack, int position, char *needle)
  *Return: the address of the beginning of theh occurence
  */
 char *_strstr(char *s, char *needle)
-{	
-	int X;
-	
-	X = 0;	
-	while (s[X] != '\0')
+{
+	for (; *s != '\0' ; s++)
 	{
-		if (s[X] == needle[0])
+		char *X = s;
+		char *Y = needle;
+		while (*X == *Y && * Y != '\0')
 		{
-			if (checker(s,X, needle ) == 1)
-			{
-				return (&s[X]);
-			}
-		}	
-		X++;
-	}	
-	return(NULL);
+			X++;
+			Y++;
+		}
+		if(*Y == '\0')
+			return (s);
+	}
+	return (NULL);
 }
