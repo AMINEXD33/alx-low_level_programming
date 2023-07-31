051aad8cf7caae6cb29ead2b67e47682d708601a
Amine Meftah <amine.gamer222@gmail.com>
2023-07-31 19:24:05 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 /**
- * checker - checks is the the following bytes are exactly equal to needle
- *@haystack: string we're searching in
- *@position: is the position of the first needle byte
- *@needle: is the needle word ,, aka what we're searching for
- *Return: 0 if needle does not eq to the string , starting at position
- */
+* checker - checks is the the following bytes are exactly equal to needle
+*@haystack: string we're searching in
+*@position: is the position of the first needle byte
+*@needle: is the needle word ,, aka what we're searching for
+*Return: 0 if needle does not eq to the string , starting at position
+*/
 int checker(char *haystack, int position, char *needle)
 {
 	int x;
@@ -15,7 +16,7 @@ int checker(char *haystack, int position, char *needle)
 
 	flag = 1;
 	x = 0;
-	while (needle[x] != '\0' && haystack [x + position]!= '\0')
+	while (needle[x] != '\0' && haystack[x + position] != '\0')
 	{
 		if (haystack[position + x] != needle[x])
 		{
