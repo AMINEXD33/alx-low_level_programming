5ac94ace8e213c2869b4a502bea8d46235b0d01c
Amine Meftah <amine.gamer222@gmail.com>
2023-07-31 11:48:54 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * *_strchr - finds the first accurence of char in a string
+ * *_strchr - finds the first occurence of char in a string
  *@s: string
  *@c: target char
- *Return: (ADRESS) if c is found , (NUL) if the target is not in the string
+ *Return: (ADRESS) if c is found , (NULL) if the target is not in the string
  *
  */
 char *_strchr(char *s, char c)
@@ -19,6 +19,10 @@ char *_strchr(char *s, char c)
 			return (&s[I]);
 		}
 		I++;
+	}
+	if (c == '\0' && s[I] == '\0')
+	{
+		return (&s[I]);
 	}
 	return (NULL);
 }
