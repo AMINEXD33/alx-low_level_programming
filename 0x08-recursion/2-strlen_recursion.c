6851390a91fe2fb23c7838575ca8f67393bee7bf
Amine Meftah <amine.gamer222@gmail.com>
2023-08-02 10:46:49 +0100

    recursion, length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	static int count;
-
 	if (*s ==  '\0')
 	{
-		return (count);
+		return (0);
 	}
-	count++;
-	_strlen_recursion(s + 1);
-	return (count);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
