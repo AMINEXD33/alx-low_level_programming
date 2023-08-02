0ef0d38d2760d707dd9441a79a6d1ffeb899c3e2
Amine Meftah <amine.gamer222@gmail.com>
2023-08-02 10:07:07 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts_recursion - prints a char with recursion
+ *@s: char array
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
