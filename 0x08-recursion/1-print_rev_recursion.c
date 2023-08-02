62dff5fabe681ead1a6eb83ac781ab3a5f0c43fa
Amine Meftah <amine.gamer222@gmail.com>
2023-08-02 10:16:06 +0100

    recursion, reverse a char

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ *_print_rev_recursion- print a string in reverce
+ *@s: string
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\0');
+		_putchar('\n');
 		return;
 	}
 	_print_rev_recursion(s + 1);
