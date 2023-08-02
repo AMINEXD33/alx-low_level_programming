dd3413a68b13556dcb8a2e68cb964e1436ebb429
Amine Meftah <amine.gamer222@gmail.com>
2023-08-02 18:14:07 +0100

    X power if Y

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ *helper - binary search to get to naturale square root of n
+ *@n: number
+ *@start: start position
+ *@end: end position
+ *Return: (Naturale sq root of n )
+ */
+int helper(int n, int start, int end)
+{
+	if (start <= end)
+	{
+		int mid;
+		int sq;
+
+
+		mid = start + (end - start) / 2;
+		sq = mid * mid;
+		if (sq == n)
+		{
+			return (mid);
+		}
+		else if (sq > n)
+		{
+			return (helper(n, start, mid - 1));
+		}
+		else
+		{
+			return (helper(n, mid + 1, end));
+		}
+	}
+	return (-1);
+}
+/**
+ *_sqrt_recursion - handle the case of n<0 , and start the binary search
+ *@n: number
+ *Return: the final result form help function
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (helper(n, 0, n));
+}
