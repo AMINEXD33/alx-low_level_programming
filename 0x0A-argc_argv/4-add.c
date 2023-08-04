9ba0b707a26931714a73740cc3f1be131056f6d0
Amine Meftah <amine.gamer222@gmail.com>
2023-08-04 21:17:34 +0100

    add positive

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main- Enry point
+ *@argc: length of argv
+ *@argv: args vercor
+ *Return: 1 if a character is not a digit, else 0
+ */
+int main(int argc, char *argv[])
+{
+	int SUM;
+	int num;
+
+
+	SUM = 0;
+	argv++; /*skiping the prog name*/
+	while (*argv != NULL)
+	{
+		if (isdigit(**argv))
+		{
+			num = atoi(*argv);
+			if (num >= 0)
+			{
+				SUM += num;
+			}
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		argv++;
+	}
+	argc = argc;
+	printf("%d\n", SUM);
+	return (0);
+}
