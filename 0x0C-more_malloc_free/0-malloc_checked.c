c161d5c927a959031712fd93ca74a7a30950750f
Amine Meftah <amine.gamer222@gmail.com>
2023-08-10 11:19:52 +0100

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,9 @@
 #include "stdlib.h"
 #include "stddef.h"
 /**
-*
-*
-*
+*malloc_checked- allocate b bytes
+*@b: bytes value
+*Return: address of the allocated memory
 */
 void *malloc_checked(unsigned int b)
 {
