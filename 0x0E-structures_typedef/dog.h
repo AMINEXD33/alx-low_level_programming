fed5e399c96c26b6baabdac2b93d68c655370d9f
Amine Meftah <amine.gamer222@gmail.com>
2023-08-14 14:59:54 +0100

    forgot readme

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-struct dog 
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- its a struct for DOGGO
+ *@name: name of doggo
+ *@owner: the name of the owner of doggo
+ *@age: age of doggo
+ */
+struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 }
+#endif /* DOG_H*/
