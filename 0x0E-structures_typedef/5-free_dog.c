76a27a78f0341bad09f8362fc2de19737c863e82
Amine Meftah <amine.gamer222@gmail.com>
2023-08-14 17:08:47 +0100

    fixed print dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *free_dog- free a struct (dog)
+ *@d: struct
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
 }
