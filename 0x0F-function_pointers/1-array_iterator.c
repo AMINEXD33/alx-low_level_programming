3d33a394d72abd759e7617a4b0451af3937c1865
Amine Meftah <amine.gamer222@gmail.com>
2023-08-16 14:50:25 +0100

    functions pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,22 @@
 #include "function_pointers.h"
 #include <stddef.h>
 /**
-*
-*
-*
-*
+*array_iterator-calls a nother function with each element in array
+*@array: an array of ints
+*@size: size of array
+*@action: address to the other function
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-
-	x = 0;
-	while (x < size)
+	if (array != NULL)
 	{
-		action(array[x]);
-		x++;
+		x = 0;
+		while (x < size)
+		{
+			action(array[x]);
+			x++;
+		}
 	}
 }
