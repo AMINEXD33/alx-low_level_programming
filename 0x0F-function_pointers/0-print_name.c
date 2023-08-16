50d4dad475045dd8a4d4a3c21f3dc3e52f4c66f8
Amine Meftah <amine.gamer222@gmail.com>
2023-08-16 14:16:34 +0100

    prepro

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+*print_name-a function to a function
+*@name: the name
+*@f: address of the function
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
