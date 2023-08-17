4411989f7640ab853fe12f0427dd733b11971fa8
Amine Meftah <amine.gamer222@gmail.com>
2023-08-17 12:48:27 +0100

    variadic stuf :3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,3 +2,13 @@
 #define _0sum_them_all_H
 int sum_them_all(const unsigned int n, ...);
 #endif /*_0sum_them_all_H*/
+
+#ifndef _1print_numbers_H
+#define _1print_numbers_H
+void print_numbers(const char *separator, const unsigned int n, ...);
+#endif /*_1print_numbers_H*/
+
+#ifndef _putchar_H
+#define _putchar_H
+int _putchar(char c);
+#endif /*_putchar_H*/
