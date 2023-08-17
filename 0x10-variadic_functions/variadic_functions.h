f8f875293024c33b58ec17133a8a3074108b17e5
Amine Meftah <amine.gamer222@gmail.com>
2023-08-17 17:32:58 +0100

    variadic stuf :3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,37 +1,23 @@
-
-#ifndef _0sum_them_all_H
-#define _0sum_them_all_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
-#endif /*_0sum_them_all_H*/
-
-#ifndef _1print_numbers_H
-#define _1print_numbers_H
 void print_numbers(const char *separator, const unsigned int n, ...);
-#endif /*_1print_numbers_H*/
-
-#ifndef _putchar_H
-#define _putchar_H
 int _putchar(char c);
-#endif /*_putchar_H*/
-
-#ifndef _2print_strings_H
-#define _2print_strings_H
 void print_strings(const char *separator, const unsigned int n, ...);
-#endif /*print_strings*/
-
-#ifndef _3print_all_H
-#define _3print_all_H
 void print_all(const char * const format, ...);
+
 /**
  * struct choice- the type and its correct printing function
  * @type: the type , char , int , float , string
  * @f:the correct function to print the type
  */
-#include <stdarg.h>
 typedef struct
 {
 	char type;
 	void(*f)(va_list);
 } choice;
-#endif /*_3print_all*/
+
+#endif /*VARIADIC_FUNCTIONS_H*/
 
