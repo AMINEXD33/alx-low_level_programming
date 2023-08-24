6c7570b674f316f44ff3a401cf8efa1c7ee97716
Amine Meftah <amine.gamer222@gmail.com>
2023-08-24 14:47:37 +0100

    print num of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len-count the elements in a single linked list
+ * @h: header
+ * Return: the num of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int x;
+
+	while (h != NULL)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x)
+}
