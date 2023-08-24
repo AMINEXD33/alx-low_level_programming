084d4b1179b441c3c3abebb60756709e598f13d4
Amine Meftah <amine.gamer222@gmail.com>
2023-08-24 17:06:29 +0100

    print befor the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-void print_befor(void) __attribute__((constroctor));
+void print_befor(void) __attribute__((constructor));
 /**
  * print_befor-a function that prints befor the main function
  */
 void print_befor(void)
 {
-	printf("You're beat! and yet, you must
-	allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must ");
+	printf("allow,\nI bore my house upon my back!\n");
 }
