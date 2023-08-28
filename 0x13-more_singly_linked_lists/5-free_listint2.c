90a0ce3bd713eff30bd3aab3fd291dbf45f3b9a5
Amine Meftah <amine.gamer222@gmail.com>
2023-08-28 18:17:52 +0100

    freeing a lnked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint-free a linked list
+ * @head: is the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
+/**
+ * free_listint2-free a list and null the head
+ * @head: the head of the linked list
+ */
+void free_listint2(listint_t **head)
+{
+	free_listint((*head));
+	(*head) = NULL;
+}
+
