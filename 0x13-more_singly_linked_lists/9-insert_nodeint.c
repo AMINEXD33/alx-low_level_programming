5fc87e4ce13bdeebd0ad2eae201c983ea104d357
Amine Meftah <amine.gamer222@gmail.com>
2023-08-28 20:13:31 +0100

    insert a node at an index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	TMP_NODE = (*head);
 	target = 0;
 	flag = 0;
-	while (TMP_NODE->next != NULL && target <= idx) 
+	while (TMP_NODE->next != NULL) 
 	{
 		if (target == idx)
 		{
