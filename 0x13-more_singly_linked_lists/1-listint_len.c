5d5fdfbff62ca761386e367e7a160b254ff2b5ab
Amine Meftah <amine.gamer222@gmail.com>
2023-08-28 16:49:12 +0100

    count elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len-count elements in a linked list
+ * @h: head of the linked list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int elements;
+
+	if (h == NULL)
+		return (0);
+	elements = 0;
+	while (h->next != NULL)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
