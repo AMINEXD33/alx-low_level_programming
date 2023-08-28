27a3e6101bd83bd6923d7872f0b1f5d07e4bc8ac
Amine Meftah <amine.gamer222@gmail.com>
2023-08-28 23:35:45 +0100

    insert a node at an index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		TMP_NODE = TMP_NODE->next;
 		if (TMP_NODE == NULL)
+		{
+			free(NODE);
 			return (NULL);
+		}
 	}
 	if (idx == 0)
 	{
