031125bcee709a88a2c132a3087138b43de4e77d
Amine Meftah <amine.gamer222@gmail.com>
2023-08-31 23:02:22 +0100

    change a bit to 1 at an index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,12 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
+	if (index > (sizeof(unsigned long int) * sizeof(unsigned long int)))
+		return (-1);
 	/*make a mask,0x01 shifted by index*/
 	mask = 1 << index;
 	/*applly the mask to the number*/
 	*n |= mask;
 
-	return(*n);
+	return(1);
 }
