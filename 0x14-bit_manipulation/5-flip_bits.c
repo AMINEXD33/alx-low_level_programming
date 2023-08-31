f2d5b63dc765bd61d91a076b8fce02c9b1cf2586
Amine Meftah <amine.gamer222@gmail.com>
2023-08-31 23:30:07 +0100

    clear bit at index

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * flip_bits- returns the number of bits you would need to flip to get from 
+ * one number to another.
+ * @n: the number
+ * @m: the the number you need to flip
+ * 
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int Y = 0;
+	unsigned long int X;
+
+	/*xor n and b*/
+	X = n ^ m;
+	/*keep shifting diff to right until it gets to 0*/
+	do {
+
+		Y += (X & 1);
+		X >>= 1;
+	} while (X > 0);
+
+	return (Y);
+}
