1c60d5636eefd0aa48bbc5320e0ef905e960caf7
Amine Meftah <amine.gamer222@gmail.com>
2023-09-04 19:21:05 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,8 @@ int create_file(const char *filename, char *text_content)
 	mode_t FILE_PERMITIONS;
 
 	len = strlen(text_content);
+	if (len == 0)
+		return (1);
 	/*read and write permission for the owner*/
 	FILE_PERMITIONS = S_IRUSR | S_IWUSR;
 	if (filename == NULL)
@@ -34,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 		if (file == -1)
 			return (-1);
 	}
-	if (flag == 0 && len > 0)/*it exists && len of content is greater than 0*/
+	if (flag == 0 && len > 0)/*exists*/
 	{
 		trunc_code = ftruncate(file, len);
 		if (trunc_code != 0)
@@ -43,7 +45,7 @@ int create_file(const char *filename, char *text_content)
 		if (write_code == -1)
 			return (-1);
 	}
-	else if (flag == 1 && len > 0)/* !exists && len > 0*/
+	else if (flag == 1)/* !exists*/
 	{
 		write_code = write(file, text_content, len);
 		if (write_code == -1)
