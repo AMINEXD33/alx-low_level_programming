94e7c8d2d03bff9e02a99f9a6ddd53f8bcf42b73
Amine Meftah <amine.gamer222@gmail.com>
2023-09-05 00:54:54 +0100

    append to a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,4 +3,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+typedef struct BUFFER
+{
+	int size;
+	char *string;
+}buffer;
 #endif /**_IO_H**/
