e8a14ec5bdc67b63c5e3698ba20f7df1cd778450
Amine Meftah <amine.gamer222@gmail.com>
2023-09-05 13:47:32 +0100

    read from a file and write to STDOUT

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,9 +3,14 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+/**
+ *struct BUFFER- SIMPLE buffer handling structure
+ *@size: the size of the string
+ *@string: a string
+ */
 typedef struct BUFFER
 {
 	int size;
 	char *string;
-}buffer;
+} buffer;
 #endif /**_IO_H**/
