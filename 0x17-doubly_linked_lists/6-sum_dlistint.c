2c8ddc4b7b26be6da450783ea8ddc17a31c8c0ef
Amine Meftah <amine.gamer222@gmail.com>
2023-10-12 20:03:04 +0100

    print the sum of a list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * 
- * 
- * 
- * 
+ * sum_dlistint-sum up the list values
+ * @head: the head of the list
+ * Return: the sum
  */
 int sum_dlistint(dlistint_t *head)
 {
@@ -12,6 +11,8 @@ int sum_dlistint(dlistint_t *head)
 	int sum;
 
 	sum = 0;
+	if (node == NULL)
+		return (sum);
 	/*checking if we're at the start of the list*/
 	while (node->prev != NULL)
 		node = node->prev;
