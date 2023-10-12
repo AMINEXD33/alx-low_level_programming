d13145e4152a9bbeae095dff751bf807a233b755
Amine Meftah <amine.gamer222@gmail.com>
2023-10-12 19:37:34 +0100

    free linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * 
- * 
- * 
- * 
- * 
+ * free_dlistint-free a linked list
+ * @head: the head of the linked list
  */
 void free_dlistint(dlistint_t *head)
 {
