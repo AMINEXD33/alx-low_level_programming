dd125c644f13974a9516ae0acb4c307554830843
Amine Meftah <amine.gamer222@gmail.com>
2023-10-12 23:08:55 +0100

    insert at X

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 
- * 
- * 
- * 
- * 
+ * insert_dnodeint_at_index- insert a node at an index (idx)
+ * @h: the head of the list
+ * @idx: the targeted index
+ * @n: the new node val
+ * Return: the addr of the new added node , NULL if it fails
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || curr_n == NULL)
 		return (NULL);
 	/*verifie position*/
 	while (curr_n->prev != NULL)
@@ -23,28 +23,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/*if inserting into head*/
 	if (idx == 0)
 	{
-		curr_n = add_dnodeint(&curr_n,(const int)n);
-		return(curr_n);
+		curr_n = add_dnodeint(&curr_n, (const int)n);
+		*h = curr_n;
+		return (curr_n);
 	}
 	/*search for the right index*/
 	while (idx != 0 && curr_n != NULL)
 	{
 		prev_n = curr_n;
 		curr_n = curr_n->next;
-		idx--;	
+		idx--;
 	}
-	/*if it's idx pointing to the last element*/
-	if (prev_n->next == NULL)
+	/*if curr_n , thus index is in range*/
+	if (curr_n != NULL)
 	{
-		curr_n = add_dnodeint_end(&curr_n,(const int)n);
+		/*if it's idx pointing to the last element*/
+		if (curr_n != NULL && curr_n->next == NULL)
+		{
+			curr_n = add_dnodeint_end(&curr_n, (const int)n);
+			return (curr_n);
+		}
+		new_node->n = n;/*conf new node*/
+		/*if idx is not at the end or begining of the list*/
+		new_node->next = curr_n, new_node->prev = prev_n;
+		curr_n->prev = new_node, prev_n->next = new_node;
+		return (new_node);
 	}
-	/*conf new node*/
-	new_node->n = n;
-	/*if idx is not at the end or begining of the list*/
-	new_node->next = curr_n;
-	new_node->prev = prev_n;
-
-	curr_n->prev = new_node;
-	prev_n->next = new_node;
-	return(new_node);
+	return (NULL);/*out of range , returning NULL*/
 }
