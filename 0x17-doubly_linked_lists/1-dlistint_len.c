38fc790993c765ad19ab9d6957a64a1ef8951a8d
Amine Meftah <amine.gamer222@gmail.com>
2023-10-12 18:15:10 +0100

    lenght of the D_linkedlist

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,26 +1,28 @@
 #include "lists.h"
 /**
- * dlistint_len-print the values of nodes in a linked list
- * @h: head of the linked list
- * Return: the size of the linked list
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	const dlistint_t *node = h;
 	int count;
 
 	count = 0;
-	if (node == NULL)
+
+	if (h == NULL)
 		return (count);
 
-	while (node->prev != NULL)
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		node = node->prev;
-	}
-	while (node != NULL)
-	{
-		node = node->next;
 		count++;
+		h = h->next;
 	}
+
 	return (count);
 }
