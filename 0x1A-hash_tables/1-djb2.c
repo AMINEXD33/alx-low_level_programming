7a6759a1e1689e85ba169e06c87739ebe1e8c5cd
Amine Meftah <amine.gamer222@gmail.com>
2023-11-02 14:51:14 +0100

    the hashing function

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_djb2- using djb2 algorithm to generate
+ * a hash for a giving string
+ * @str: the string
+ * Return: the hashed value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
