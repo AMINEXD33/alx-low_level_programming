19f116829a805fef1102e5fb140be44273e85ccc
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-05 22:11:23 +0100

    task1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned int tracker = 0;
 
-	if (array == NULL || value == NULL)
+	if (array == NULL)
 		return (-1);
 	while (tracker < size)
 	{
