e1399e7fa33925aef6274c011c0b66426184b83d
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-06 19:14:50 +0100

    jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ *jump_search- a function that performs a search using a jump
+ *search , O(sqr(n))
+ *@array: the array we're searching
+ *@size: the size of the array
+ *@value: the value we're searching for
+ *Return: the index of the element or -1 if it doesn't exist
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t x;
+	size_t y;
+	size_t tmp;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x += sqrt(size))
+	{
+		/*check if this is the value*/
+		/**if (array[x] == value)**/
+			/**return (x);**/
+		/*check if we're ahead of the element*/
+		if (array[x] >= value)
+		{
+			tmp = (x - sqrt(size) + 1);
+			printf("Value found between indexes ");
+			printf("[%ld] and [%ld]\n", tmp, x);
+			/*the element must be betwen X-sqrt(x) and X*/
+			for (y = tmp; y <= x ; y++)
+			{
+				printf("Value checked array");
+				printf("[%ld] = [%d]\n", y, array[y]);
+				if (array[y] == value)
+					return (y);
+			}
+			/*not found in the above intervale, so it doesn't exist*/
+			return (-1);
+		}
+		else
+		{
+			printf("Value checked array");
+			printf("[%ld] = [%d]\n", x, array[x]);
+		}
+	}
+	return (-1);
+}
