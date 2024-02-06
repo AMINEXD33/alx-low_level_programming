a99cf283bc134eec0931429e399c6f09b55c28c0
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-06 19:20:30 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t y;
 	size_t tmp;
 
-	if (array == NULL || !value)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (x = 0; x < size; x += sqrt(size))
